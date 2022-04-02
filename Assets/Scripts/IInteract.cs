63d69ff5661b0d8c9cc856c72fcc0c458e9c8424
Nathaniel Girard <nathaniel.girard1@gmail.com>
2022-04-01 23:32:17 -0400

    Player script

---
+++ Assets/Scripts/IInteract.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteract
+{
+    // Start is called before the first frame update
+
+}
+
+
+
+public interface IInteractable
+{
+    public abstract void Interact();
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using General;
+
+public class Player : MonoBehaviour, IUpdaptable
+{
+    public void FixedRefresh()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void PostInit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Refresh()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
