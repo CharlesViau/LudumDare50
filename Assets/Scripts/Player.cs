9c38b8b14a53fd21fb6eccc64538fe09323a1146
Nathaniel Girard <nathaniel.girard1@gmail.com>
2022-04-02 17:26:25 -0400

    UserInput and some of Respawn()

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,14 @@ public class Player : MonoBehaviour, IUpdatable, IInteract
 {
     public Inventory Inventory { get; set; }
     [SerializeField] private int inventoryCapacity;
+    //ajouté par Nath
+    public Rigidbody rb;
+    //le premier étage correspond à currentStage = 0;
+    public int currentStage = 1;
+    //liste contenant les positions des 4 empty "PlayerSpawn"; il faudrait l'initialiser dans Init(); Respawn() utilise cette liste pour modifier la position du joueur lors des changements d'étage
+    private List<Vector3> spawnTransforms;
+
+    private string staircaseUsed = "up";
 
     public void FixedRefresh()
     {
@@ -28,5 +36,34 @@ public class Player : MonoBehaviour, IUpdatable, IInteract
     public void Interact()
     {
         
+    }
+
+    public void upStage()
+    {
+        if (currentStage <= 1)
+        {
+            currentStage += 1;
+            staircaseUsed = "up";
+            Respawn();
+        }
+        //faire monter d'étage; pas de else, on est juste con si la fonction est mal utilisée
+    }
+    
+    public void downStage()
+    {
+        if (currentStage >= 1)
+        {
+            currentStage -= 1;
+            staircaseUsed = "down";
+            Respawn();
+        }
+        //faire descendre d'étage; pas vraiment besoin de else ici non plus je crois
+    }
+
+    public void Respawn()
+    {
+
+
+        //faire réapparaître le joueur à l'étage correspondant à currentStage et à l'escalier correspondant à staircaseUsed
     }
 }
