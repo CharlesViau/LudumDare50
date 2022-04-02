97c3d63e7d0920bdbdcdde98370998323b79896d
Matthew Verret <MAVER194@ulaval.ca>
2022-04-02 11:59:51 -0400

    New prefabs

--- Assets/Scripts/MainMenuComponent.cs
+++ Assets/Scripts/MainMenuComponent.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuComponent : MonoBehaviour
 {
+    /// <summary>
+    /// Makes us continue on the next scene in the build setting
+    /// </summary>
     public void OnPlayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Quit the game
+    /// </summary>
+    public void OnQuitButton()
+    {
+        Application.Quit();
     }
 }
