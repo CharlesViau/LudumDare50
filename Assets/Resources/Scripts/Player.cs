546bf09cc319220ee1005caae6174bab27724ad3
Charles_Viau <charles_violon@hotmail.com>
2022-04-03 02:40:46 -0500

    LES OSTI DE CONTROLE SONT FINI

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -8,50 +8,65 @@ using General;
 public class Player : MonoBehaviour, IInteract, IMove
 {
     public Inventory Inventory { get; set; }
-    private UserInput _input;
-    private AnimationStatePlayer _animationStatePlayer;
     [SerializeField] private int inventoryCapacity;
-  
-    public Rigidbody rb;
-    public Animator animator;
-    
-    [SerializeField]public float speed = 5;
+    private Transform _transform1;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Animator animator;
+
+    [SerializeField] public float speed;
+    [SerializeField] public float turningSpeed;
+    private float _turnSmoothVelocity;
+
+    private readonly int _speedHash = Animator.StringToHash("Speed");
+
+    private const string InteractAction = "Fire1";
+    private const string Vertical = "Vertical";
+    private const string Horizontal = "Horizontal";
 
     private void Awake()
     {
         Inventory = new Inventory(inventoryCapacity);
-        _input = new UserInput(this);
-        _animationStatePlayer = new AnimationStatePlayer(this);
-        
-        _input.Init();
-        _animationStatePlayer.Init();
+        _transform1 = transform;
     }
 
     private void Start()
     {
-        _input.PostInit();
-        _animationStatePlayer.PostInit();
     }
 
     private void Update()
     {
-        _input.Refresh();
+        if (Input.GetButtonDown(InteractAction))
+        {
+            Interact();
+        }
+        
+        
     }
 
     private void FixedUpdate()
     {
-        _input.FixedRefresh();
+        if (new Vector3(Input.GetAxis(Horizontal), 0, Input.GetAxis(Vertical)).normalized is var direction &&
+            direction.magnitude >= 0.1f)
+        {
+            Move(direction);
+            Debug.Log(animator.GetFloat(_speedHash));
+        }
+        
+        animator.SetFloat(_speedHash, direction.normalized.magnitude/speed);
     }
 
     public void Interact()
     {
-        
     }
 
-    public void Move(Vector3 move)
+    public void Move(Vector3 direction)
     {
-        rb.AddForce(move * speed);
-    }
+        var smoothAngle =
+            Mathf.SmoothDampAngle(_transform1.eulerAngles.y, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg,
+                ref _turnSmoothVelocity, turningSpeed);
 
-   
+        _transform1.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+        rb.MovePosition(_transform1.position + direction * speed * Time.deltaTime);
+    }
 }
