78585288ef9043f8550565db6297ffffb07a769c
Nathaniel Girard <nathaniel.girard1@gmail.com>
2022-04-03 22:05:33 -0400

    Useless code

--- Assets/Resources/Scripts/TimeManager.cs
+++ Assets/Resources/Scripts/TimeManager.cs
@@ -15,10 +15,8 @@ public class TimeManager : MonoBehaviour
     private float _initialTime;
 
     public static float _elapsedTime = 0f;
-    [SerializeField] public GameObject Water;
     [SerializeField] public TextMeshProUGUI timeText;
     [SerializeField] public float holeScaling = 1.002f;
-    [SerializeField] private int waterDelta = 5;
     // Start is called before the first frame update
 
     private void Awake()
@@ -49,10 +47,7 @@ public class TimeManager : MonoBehaviour
 
         //make water go up according to time
         //the function I made is complete trash - if currentTime goes above initialTime the water goes down and everything fucks up
-        foreach (Transform child in Water.transform)
-        {
-            child.transform.position -= new Vector3(0, waterDelta * (_initialTime - _remainingTime) / _initialTime, 0);
-        }
+        
         
 
         //make maxHoles scale up as time advances
