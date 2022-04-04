668543b83b28609fcb749736aa005023cb349cf8
Matthew Verret <MAVER194@ulaval.ca>
2022-04-03 22:09:37 -0400

    ScoreFinally done

--- Assets/Resources/Scripts/ScoreEndGame.cs
+++ Assets/Resources/Scripts/ScoreEndGame.cs
@@ -3,15 +3,15 @@ using TMPro;
 public class ScoreEndGame : MonoBehaviour
 {
     public TextMeshProUGUI scoreTime;
-    private float timeDuration = KeepOvertime.timeDuration;
+    private float timeScoreDuration = KeepOvertime.timeDuration;
     private int seconds;
     private void Awake()
     {
-        seconds = (int)timeDuration % 60;
+        seconds = (int)timeScoreDuration % 60;
     }
     // Start is called before the first frame update
     void Start()
     {
-        scoreTime.text = "You have delayed the inevitable for " + ((int)timeDuration / 60).ToString() + ":" + (seconds < 10 ? "0" : "") + (seconds.ToString());
+        scoreTime.text = "You have delayed the inevitable for " + ((int)timeScoreDuration / 60).ToString() + ":" + (seconds < 10 ? "0" : "") + (seconds.ToString());
     }
 }
